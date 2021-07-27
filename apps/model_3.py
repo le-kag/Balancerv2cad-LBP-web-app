05f3f82b4954dd541e935168bf8a2bd428367241
le-kag <76808956+le-kag@users.noreply.github.com>
2021-07-27 13:08:05 +0200

    Update model_3.py

--- apps/model_3.py
+++ apps/model_3.py
@@ -20,5 +20,5 @@ def app():
         y='balance_of_Token_A',
         color='par_weight_token_B',
         animation_frame='par_weight_token_B',
-        height=450, width=1000)
+        height=450, width=800)
     st.plotly_chart(fig3)
