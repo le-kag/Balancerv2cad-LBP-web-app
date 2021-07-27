ca4b34d5244eb11fd19ff3084315ec9f90759c2f
le-kag <76808956+le-kag@users.noreply.github.com>
2021-07-27 13:07:10 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -11,11 +11,26 @@ def app():
 
     st.write("""
     ### Using the balancerV2cad model we run a sensitivity analysis with cadCAD. Moreover we created a streamlit application that displays the results. With this tool we would like to help newcomers to easily get started with balancer and LBPs.
+
+
     """)
 
-    st.image(
-            'https://i.imgur.com/rkF7Vm9.png'
-        )
-    st.image(
-            'https://i.imgur.com/XbafiLj.png',
-        )
+    st.write("For further information on our research, read our HackMD available here: [link](https://hackmd.io/00CXctAPTZmHWSjBmbXtgw?view)")
+
+    col1, col2 = st.beta_columns(2)
+
+    with col1:
+        st.write("""
+
+        """)
+        st.image(
+                'https://i.imgur.com/rkF7Vm9.png'
+            )
+
+    with col2:
+        st.write("""
+
+         """)
+        st.image(
+            'https://i.imgur.com/XbafiLj.png'
+            )
