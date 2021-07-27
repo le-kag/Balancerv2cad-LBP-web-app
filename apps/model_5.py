629fcc9998c9353d47c110dcd0f5a4ae2026d7f1
le-kag <76808956+le-kag@users.noreply.github.com>
2021-07-27 13:08:37 +0200

    Update model_5.py

--- apps/model_5.py
+++ apps/model_5.py
@@ -18,5 +18,5 @@ def app():
         y='balance_of_Token_A',
         color='par_swap_fee',
         animation_frame='par_swap_fee',
-        height=450, width=1000)
+        height=450, width=800)
     st.plotly_chart(fig5)
